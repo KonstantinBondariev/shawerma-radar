cec39d20ae69495a20bbe633b28eda6f0b4e79ad
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2023-04-13 23:12:36 +0300

    map.arrowsPosition()

--- src/app/modules/map/components/map/map.component.ts
+++ src/app/modules/map/components/map/map.component.ts
@@ -30,8 +30,10 @@ import {
 
 import { GeolocationService } from 'src/app/services/geolocation-service.service';
 import { userData } from 'src/app/modules/shared/data/userData';
-import 'leaflet-rotatedmarker';
 import 'leaflet-geodesy';
+import 'leaflet-rotatedmarker';
+
+import * as turf from '@turf/turf';
 
 @Component({
   selector: 'app-map',
@@ -90,24 +92,28 @@ export class MapComponent implements OnInit, OnDestroy {
 
   popupRollText = 'Shawerma';
 
-  markerRollIcon = {
-    icon: L.icon({
-      iconSize: [15, 32],
-      iconAnchor: [7.5, 32],
-      popupAnchor: [2, -40],
-      // specify the path here
-      iconUrl: 'assets/img/roll.png',
-      // shadowUrl:
-      //   'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
-    }),
-  };
-
   public map!: Map;
   public zoom!: number;
 
   public line!: any;
   public prevLine!: any;
   lines: any[] = [];
+
+  // angleInDegrees: number = 0;
+  arrows: any[] = [];
+
+  markerRollIcon = {
+    icon: L.icon({
+      iconSize: [15, 32],
+      iconAnchor: [7.5, 32],
+      popupAnchor: [2, -40],
+
+      // specify the path here
+      iconUrl: 'assets/img/roll.png',
+      // shadowUrl:
+      //   'https://unpkg.com/leaflet@1.4.0/dist/images/marker-shadow.png',
+    }),
+  };
 
   constructor(private geolocationService: GeolocationService) {}
 
@@ -195,21 +201,59 @@ export class MapComponent implements OnInit, OnDestroy {
         center,
         L.latLng(doner.coordinates.lat, doner.coordinates.lon),
       ];
-      const polyline = L.polyline(latLngs, { color: 'red' }).addTo(map);
+      const polyline = L.polyline(latLngs, { color: 'null' }).addTo(map);
       this.lines.push(polyline);
     });
+    this.arrowsPosition(this.lines);
   }
 
-  lineF(doner: {
-    name: string;
-    coordinates: { lat: number; lon: number };
-    rating: string;
-  }) {
-    // создаем объекты L.LatLng для начальной и конечной точек линии
-    const start = L.latLng(this.userСoordinates.lat, this.userСoordinates.lon);
-    const end = L.latLng(doner.coordinates.lat, doner.coordinates.lon);
+  arrowsPosition(lines: L.Polyline[]) {
+    if (this.arrows) this.arrows.forEach((arrow) => arrow?.remove());
 
-    // создаем линию, соединяющую начальную и конечную точки
-    const polyline = L.polyline([start, end], { color: 'red' }).addTo(this.map);
+    lines.forEach((line) => {
+      const startP: any = line.toGeoJSON().geometry.coordinates[0];
+      const finishP: any = line.toGeoJSON().geometry.coordinates[1];
+
+      let vector = [];
+      vector.push(finishP[0] - startP[0]);
+      vector.push(finishP[1] - startP[1]);
+
+      const angleInDegrees: Number =
+        Math.atan2(vector[0], vector[1]) * (180 / Math.PI);
+
+      const markerArrow = {
+        icon: L.icon({
+          iconUrl: 'assets/img/arrow2.png',
+          iconSize: [100, 100],
+          iconAnchor: [50, 50],
+          // popupAnchor: [-3, -76],
+          // shadowUrl: 'my-icon-shadow.png',
+          // shadowSize: [68, 95],
+          // shadowAnchor: [22, 94],
+        }),
+        title: 'hhgj',
+        rotationAngle: angleInDegrees,
+        rotationOrigin: 'center',
+      };
+
+      const lineGeojson: any = line.toGeoJSON().geometry.coordinates;
+      var line2 = turf.lineString([lineGeojson[0], lineGeojson[1]]);
+
+      const rectangleGeojson = L.rectangle(this.map.getBounds()).toGeoJSON();
+      // const polygon = turf.lineToPolygon(line2);
+
+      const intersection = turf.lineIntersect(line2, rectangleGeojson);
+
+      if (intersection) {
+        console.log(intersection);
+        const intersectionPoint = L.latLng(
+          intersection.features[0].geometry.coordinates[1],
+          intersection.features[0].geometry.coordinates[0]
+        );
+        // Делаем что-то с точкой пересечения
+        const arrow = L.marker(intersectionPoint, markerArrow).addTo(this.map);
+        this.arrows.push(arrow);
+      }
+    });
   }
 }
